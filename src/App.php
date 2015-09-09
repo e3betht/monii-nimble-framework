40946a889f5567b2298c4ce6b05cac9a2323409a
Steve Marks <steve.marks@monii.com>
2015-09-09 15:43:29 +0100

    Make makeAndRegisterServiceProviders method public

--- src/App.php
+++ src/App.php
@@ -13,7 +13,7 @@ class App
     const DEFAULT_ACTION_ATTRIBUTE_NAME = 'monii/nimble-app:action';
     const DEFAULT_PARAMETERS_ATTRIBUTE_NAME = 'monii/nimble-app:parameters';
 
-    protected function makeAndRegisterServiceProviders(Container $container, array $serviceProviderClassNames)
+    public function makeAndRegisterServiceProviders(Container $container, array $serviceProviderClassNames)
     {
         foreach ($serviceProviderClassNames as $providerClassName) {
             $provider = $container->make($providerClassName);
