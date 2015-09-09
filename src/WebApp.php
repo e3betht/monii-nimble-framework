1a4ef13839a2ad8750ec0402c459dc3b9489b583
Steve Marks <steve.marks@monii.com>
2015-09-09 18:31:17 +0100

    Add $app as a parameter to webify() method

--- src/WebApp.php
+++ src/WebApp.php
@@ -13,9 +13,9 @@ class WebApp
     const DEFAULT_ACTION_ATTRIBUTE_NAME = 'monii/nimble-app:action';
     const DEFAULT_PARAMETERS_ATTRIBUTE_NAME = 'monii/nimble-app:parameters';
 
-    public static function webify(Container $container)
+    public static function webify(App $app, Container $container)
     {
-        $container->makeAndRegisterServiceProviders($container, [
+        $app->makeAndRegisterServiceProviders($container, [
             ActionHandlerServiceProvider::class,
             NikicFastRouteServiceProvider::class,
             RelayServiceProvider::class,
